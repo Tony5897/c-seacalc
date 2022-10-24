106208d740e1b67cbd67e38a8e3b3e7c505289a9
Tony5897 <tony.martinez5897@gmail.com>
2022-10-23 23:28:40 -0700

    final commit: Basic Calculator

--- seatest.c
+++ seatest.c
@@ -5,8 +5,39 @@
 
 
 int main(int argc, char *argv[]) 
-
 {
-    printf("demo\n");
+    float valueOne;
+    float valueTwo;
+    char operator;
+    float answer;
+
+    printf("Enter Here:\n\n");
+    scanf("%f %c %f", &valueOne, &operator, & valueTwo);
+
+    switch(operator)
+        {
+        case '/': answer = valueOne/valueTwo;
+            break;
+        case '*': answer = valueOne*valueTwo;
+            break;
+        case '+': answer = valueOne+valueTwo;
+            break;
+        case '-': answer = valueOne-valueTwo;
+            break;
+        case '^': answer = pow(valueOne,valueTwo);
+            break;
+        case ' ': answer = sqrt(valueTwo);
+            break;
+        default: goto fail;
+        }
+    printf("%.9g%c%.9g = %.6g\n\n",valueOne, operator, valueTwo, answer);
+    goto exit;
+    fail:
+    printf("Invalid Calculation.\n");
+    exit:
+    return 0;
 
 }
+
+/* Quick test run and generally, reviewing the 
+many fundamentals of the C Programming langauge */
